b4c880383c5fd2b2c681451f8aabacdc48fe8cde
Pratik <prateek.langde@gmail.com>
2023-03-26 09:01:03 +0000

    devA comment updated

--- file3.java
+++ file3.java
@@ -1,6 +1,6 @@
 public class Demo{
 	public static void main(String args[]){
-		//print statement for Hello World
+		//DevA Code for Hello World
 	System.out.println("Hello World");
 //print statement for Devops
 	System.out.println("Welcome to Devops");
