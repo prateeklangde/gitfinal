b70462565f5870d0f582b1b22ecbe10e02b937c5
Pratik <prateek.langde@gmail.com>
2023-03-26 07:43:05 +0000

    Devops changes commited

--- file3.java
+++ file3.java
@@ -1,6 +1,9 @@
 public class Demo{
 	public static void main(String args[]){
 		//print statement for Hello World
-		System.out.prinln("Hello World");
+	System.out.println("Hello World");
+//print statement for Devops
+	System.out.println("Welcome to Devops");
+
 	}
 }
